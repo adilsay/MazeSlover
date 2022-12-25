25c17ca77df85010829cca9776dd50d671946d11
Adil <adilsay007@gmail.com>
2022-12-25 22:49:44 +0530

    first commit

---
+++ src/mazeSlover/mazeSlover.java
@@ -0,0 +1,13 @@
+package mazeSlover;
+
+import GUI.View;
+
+public class mazeSlover {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		View gui = new View();
+        gui.setVisible(true); //By default, the JFrame is set to be in invisible. So, we have to set the visi
+	}
+
+}
